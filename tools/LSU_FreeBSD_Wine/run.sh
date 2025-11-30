5227442a9184df4c70caa0323aa81f0e3ec68d48
Vince <31942676+XaeroVincent@users.noreply.github.com>
2025-11-30 12:31:42 +0000

    Copy openxr folder to proper subdirectory
    Copy openxr folder to proper subdirectory

--- tools/LSU_FreeBSD_Wine/run.sh
+++ tools/LSU_FreeBSD_Wine/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 __dir__="$(dirname "$(realpath "$0")")"
 export LD_32_LIBRARY_PATH="$HOME/.i386-wine-pkg/usr/local/lib"
+cp -r $HOME/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/share/openxr $HOME/.steam/tmp/FreeBSD_Proton/proton_10.0/files/share
 exec lsu-linux-to-freebsd-env "$__dir__/run.rb" "$@"
