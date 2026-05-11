d1ae91259fab1502206d2e22aef6f13624bc78fe
Vince <31942676+XaeroVincent@users.noreply.github.com>
2026-05-10 22:39:50 -0700

    Use system gstreamer
    Use system gstreamer

--- tools/LSU_FreeBSD_Wine_11/run.sh
+++ tools/LSU_FreeBSD_Wine_11/run.sh
@@ -5,6 +5,7 @@ if [ ! -f "$HOME.i386-wine-pkg/usr/local/wine-proton-11/bin/wine" ]; then
 fi
 
 __dir__="$(dirname "$(realpath "$0")")"
+export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0 # Force Steam use to system gstreamer for video decoding
 export WINEDLLOVERRIDES="vrclient,vrclient_x64=" # Bypass assert failed popup window when using Proton 11 in LSU
 export LD_32_LIBRARY_PATH="$HOME/.i386-wine-pkg/usr/local/lib" # Enable LSU to locate additional 32-bit libraries
 ln -sf /usr/local/wine-proton-11/lib/wine/x86_64-unix/wine $HOME/.steam/tmp/FreeBSD_Proton/proton_11.0/files/lib/wine/x86_64-unix
