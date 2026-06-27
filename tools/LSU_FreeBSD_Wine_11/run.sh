e02308bc8468a913b2f7a1b2521416a99b44c686
Vince <31942676+XaeroVincent@users.noreply.github.com>
2026-06-27 00:49:30 -0700

    Copy contrib folder and amdxcffx64.dll to tmp
    Copy contrib folder and amdxcffx64.dll to tmp

--- tools/LSU_FreeBSD_Wine_11/run.sh
+++ tools/LSU_FreeBSD_Wine_11/run.sh
@@ -9,5 +9,6 @@ export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0 # Force Steam use to system 
 export WINEDLLOVERRIDES="vrclient,vrclient_x64=" # Bypass assert failed popup window when using Proton 11 in LSU
 export LD_32_LIBRARY_PATH="$HOME/.i386-wine-pkg/usr/local/lib" # Enable LSU to locate additional 32-bit libraries
 ln -sf /usr/local/wine-proton-11/lib/wine/x86_64-unix/wine $HOME/.steam/tmp/FreeBSD_Proton/proton_11.0/files/lib/wine/x86_64-unix
-cp -r "$HOME/.steam/steam/steamapps/common/Proton 11.0/files/share/openxr" $HOME/.steam/tmp/FreeBSD_Proton/proton_11.0/files/share
+cp -R "$HOME/.steam/steam/steamapps/common/Proton 11.0/files/share/openxr" $HOME/.steam/tmp/FreeBSD_Proton/proton_11.0/files/share
+cp -R "$HOME/.steam/steam/steamapps/common/Proton 11.0/contrib" $HOME/.steam/tmp/FreeBSD_Proton/proton_11.0
 exec lsu-linux-to-freebsd-env "$__dir__/run.rb" "$@"
